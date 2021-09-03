e53a8d18a311a342ebf0f4c1491301411b41facc
Navneet Singh <56965092+navneetsinghnitjsr@users.noreply.github.com>
2021-09-03 11:11:27 +0530

    Create index.js
    create node server

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const hostname = "localhost"; //for host name
+const port = 3000;            // for assigning a port
+
+//creating a server
+
+const server = http.createServer((req, res) =>{
+
+	console.log(req.headers);
+
+	res.statusCode = 200; //that means connect is successfully generated 
+	res.setHeader('Content-type','text/html');
+	res.end('<html> <body><h1>Server conncetion success :) </h1></body> </html>');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`server running at http://${hostname}:${port}`);
+});
+
+//and then go to the packege.json file and add in script section (, "start" : "node index") then save both file index.js and packege.json aftr that on cammand terminal type npm strat
