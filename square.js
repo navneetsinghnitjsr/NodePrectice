79f2d758d6dbba9b717a92beb1d179a26fbd626c
Navneet Singh <56965092+navneetsinghnitjsr@users.noreply.github.com>
2021-09-03 11:13:03 +0530

    Add files via upload

---
+++ square.js
@@ -0,0 +1,9 @@
+area = (a) =>(a*a);
+perimeter = (a) => (4*a); 
+
+
+// exports.area = (a) =>(a*a);
+// exports.perimeter = (a) => (4*a); 
+
+module.exports.area = area;
+module.exports.perimeter = perimeter; 
